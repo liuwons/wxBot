b72e48f3061de97cc3f04e48c5493f8106318254
liuyang <sleshep@qq.com>
2016-05-16 15:18:58 +0800

    add exception print

--- wxbot.py
+++ wxbot.py
@@ -13,7 +13,7 @@ import urllib
 import time
 import re
 import random
-
+from traceback import format_exc
 from requests.exceptions import ConnectionError, ReadTimeout
 import HTMLParser
 
@@ -635,6 +635,7 @@ class WXBot:
                 self.schedule()
             except:
                 print '[ERROR] Except in proc_msg'
+                print format_exc()
             check_time = time.time() - check_time
             if check_time < 0.8:
                 time.sleep(1 - check_time)
